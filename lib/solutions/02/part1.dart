c523f0ef9d35b8565c15f692446d255e1596595b
Zack Campbell <zajrik@users.noreply.github.com>
2023-12-08 12:39:54 -0600

    add day 2 solutions

--- lib/solutions/02/part1.dart
+++ lib/solutions/02/part1.dart
@@ -1,6 +1,48 @@
 part of aoc;
 
 @Solution(day: 2, part: 1)
-int day2part1(String input) {
-	return 0;
+int day2part1(List<String> input) {
+	int result = 0;
+
+	for (String line in input) {
+		final [game, ...pulls] = line
+			.split(RegExp('[:;]'))
+			.map((v) => v.trim())
+			.toList();
+
+		final int gameId = int.parse(game.split(' ')[1]);
+
+		bool validGame = true;
+
+		// Check all pulls for this game
+		for (String pull in pulls) {
+			// Parse cube sets from this pull
+			final List<(int, String)> cubes = pull.split(', ').map((v) {
+				final List<String> cubeSet = v.split(' ');
+				return (int.parse(cubeSet[0]), cubeSet[1]);
+			}).toList();
+
+			// Determine if any pulled set of cubes is invalid
+			for ((int, String) cubeSet in cubes) {
+				// End this game if it's invalid
+				if (validGame == false) {
+					break;
+				}
+
+				validGame = switch (cubeSet) {
+					(> 12, 'red') => false,
+					(> 13, 'green') => false,
+					(> 14, 'blue') => false,
+					_ => true
+				};
+			}
+		}
+
+		// Add gameId to result if the game was valid
+		if (validGame) {
+			result += gameId;
+		}
+	}
+
+	return result;
 }
--- lib/solutions/02/part2.dart
+++ lib/solutions/02/part2.dart
@@ -2,5 +2,37 @@ part of aoc;
 
 @Solution(day: 2, part: 2)
 int day2part2(List<String> input) {
-	return 0;
+	int result = 0;
+
+	for (String line in input) {
+		final [_, ...pulls] = line
+			.split(RegExp('[:;]'))
+			.map((v) => v.trim())
+			.toList();
+
+		var (minRed, minGreen, minBlue) = (0, 0, 0);
+
+		// Check all pulls for this game
+		for (String pull in pulls) {
+			// Parse cube sets from this pull
+			final List<(int, String)> cubes = pull.split(', ').map((v) {
+				final List<String> cubeSet = v.split(' ');
+				return (int.parse(cubeSet[0]), cubeSet[1]);
+			}).toList();
+
+			// Find the highest value for each color in this pull
+			for ((int, String) cubeSet in cubes) {
+				switch (cubeSet) {
+					case (int red, 'red') when red > minRed: minRed = red;
+					case (int green, 'green') when green > minGreen: minGreen = green;
+					case (int blue, 'blue') when blue > minBlue: minBlue = blue;
+				}
+			}
+		}
+
+		// Add game power to result
+		result += (minRed * minGreen * minBlue);
+	}
+
+	return result;
 }
